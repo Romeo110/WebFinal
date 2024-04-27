99dacd59dc3989c0aa120b8b10806d53ab6529ab
phallaphoap <113479263+phallaphoap@users.noreply.github.com>
2024-04-27 19:51:42 -0400

    added footer

--- recommend.php
+++ recommend.php
@@ -392,9 +392,94 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
             </section>
         </div>
 
-        <footer>
-            <p>&copy; 2024 FilmFinder. All rights reserved.</p>
+        <!-- Footer -->
+        <footer class="footer">
+            <div class="background">
+                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="100%" height="100%" viewBox="0 0 1600 900">
+          <defs>
+              <linearGradient id="bg" x2="0%" y2="100%">
+                  <stop
+                      offset="0%"
+                      style="stop-color: #642b5b"
+                  ></stop>
+                  <stop
+                      offset="100%"
+                      style="stop-color: #fdf6fc"
+                  ></stop>
+              </linearGradient>
+              <path
+                  id="wave"
+                  fill="url(#bg)"
+                  d="M-363.852,502.589c0,0,236.988-41.997,505.475,0
+      s371.981,38.998,575.971,0s293.985-39.278,505.474,5.859s493.475,48.368,716.963-4.995v560.106H-363.852V502.589z"
+              />
+          </defs>
+          <g>
+              <use xlink:href="#wave" opacity=".3">
+                  <animateTransform
+                      attributeName="transform"
+                      attributeType="XML"
+                      type="translate"
+                      dur="8s"
+                      calcMode="spline"
+                      values="270 230; -334 180; 270 230"
+                      keyTimes="0; .5; 1"
+                      keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
+                      repeatCount="indefinite"
+                  />
+              </use>
+              <use xlink:href="#wave" opacity=".6">
+                  <animateTransform
+                      attributeName="transform"
+                      attributeType="XML"
+                      type="translate"
+                      dur="6s"
+                      calcMode="spline"
+                      values="-270 230;243 220;-270 230"
+                      keyTimes="0; .6; 1"
+                      keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
+                      repeatCount="indefinite"
+                  />
+              </use>
+              <use xlink:href="#wave" opacty=".9">
+                  <animateTransform
+                      attributeName="transform"
+                      attributeType="XML"
+                      type="translate"
+                      dur="4s"
+                      calcMode="spline"
+                      values="0 230;-140 200;0 230"
+                      keyTimes="0; .4; 1"
+                      keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
+                      repeatCount="indefinite"
+                  />
+              </use>
+          </g>
+          <text x="50%" y="93%" dominant-baseline="middle" text-anchor="middle" fill="white" font-size="15">
+              &copy; 2024 FilmFinder. All rights reserved.
+          </text>
+      </svg>
+            </div>
+
         </footer>
+        <script>
+            // footer swap
+            window.addEventListener('scroll', function() {
+                var scrollPosition = window.scrollY;
+                var windowHeight = window.innerHeight;
+                var documentHeight = document.documentElement.scrollHeight;
+                var threshold = 30;
+
+                // Check if the scrollbar is at the bottom of the page
+                if (scrollPosition + windowHeight >= documentHeight - threshold) {
+                    // Show animated footer
+                    document.body.classList.add('show-footer');
+                } else {
+                    // Hide animated footer
+                    document.body.classList.remove('show-footer');
+                }
+            });
+        </script>
     </body>
 
     </html>
